30edf48370cbb377726a9691096f11c3cfc992c1
dab10 <dab10@list.ru>
2022-01-06 21:44:36 +0300

    feat: add in index.js solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let reverseN = Math.abs(n) + '';
+  let result = '';  
+
+  for (i = 0; i < reverseN.length; i = i + 1) {
+    result = `${reverseN[i]}${result}`;
+  }
+  return +result;
 }
